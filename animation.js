415cec77a1a97169b1f80e8cf934ceb18dfe8bac
NunoFlorido-dev <126115463+NunoFlorido-dev@users.noreply.github.com>
2025-04-18 16:43:43 +0100

    Add cheesenotes animation

--- animation.js
+++ animation.js
@@ -1,5 +1,6 @@
 import { gsap } from "gsap";
 
+// TINKER WIKI ////////// TINKER WIKI /////// TINKER WIKI //////
 //Animate the inventions' images from TinkerWiki main page
 const inventionImages = document.querySelectorAll(
   ".mainwindow_tinkerwiki #tinkerwiki_inventory .inventory_item img"
@@ -28,6 +29,34 @@ document.addEventListener("DOMContentLoaded", () => {
 
     invention.addEventListener("mouseout", () => {
       gsap.to(invention, { scale: 1, duration: 1, ease: "back.inOut" });
+    });
+  });
+});
+
+// CHEESE NOTES ////////// CHEESE NOTES /////// CHEESE NOTES //////
+
+document.addEventListener("DOMContentLoaded", () => {
+  const cheeseNotesPreview = document.querySelectorAll(
+    ".mainwindow_cheesenotes .cheesenotes_messagepreview div"
+  );
+
+  const cheeseNotesMessages = document.querySelectorAll(
+    ".mainwindow_cheesenotes .cheesenotes_message"
+  );
+
+  cheeseNotesPreview.forEach((notePrev) => {
+    notePrev.addEventListener("mouseover", () => {
+      gsap.to(notePrev, { backgroundColor: "#ffff71", duration: 0.25 });
+
+      notePrev.addEventListener("click", () => {
+        cheeseNotesMessages.forEach((message) => {
+          gsap.to(message, { opacity: 1, duration: 0.5 });
+        });
+      });
+    });
+
+    notePrev.addEventListener("mouseout", () => {
+      gsap.to(notePrev, { backgroundColor: "#292929", duration: 0.25 });
     });
   });
 });
